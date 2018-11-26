6072d61fe042d0fdf666b1af33c50e55ea83e89e
Michel Promonet <michel.promonet.1@gmail.com>
2018-11-27 00:35:53 +0100

    fix installaion of cf

--- cf-test.sh
+++ cf-test.sh
@@ -5,5 +5,5 @@ cf login -a api.$CFDOMAIN -u admin -p $(cat <(bosh int ~/cf-creds.yml --path /cf
 
 # login to uaa
 # ========================
-uaac target uaa.$CFDOMAIN 
+uaac target uaa.$CFDOMAIN --skip-ssl-validation
 uaac token client get admin -s $(cat <(bosh int ~/cf-creds.yml --path /uaa_admin_client_secret))
