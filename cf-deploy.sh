ff6be7addd7c53b566323efe8a00c984638721fc
Michel Promonet <michel.promonet.1@gmail.com>
2019-05-05 23:10:39 +0200

    simplify

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -10,7 +10,7 @@ export NETWORK_STATIC=10.244.0.2-10.244.0.127
 export DNS="[8.8.8.8]"
 
 pushd bosh-deployment > /dev/null
-	bosh int bosh.yml \
+	bosh create-env bosh.yml \
 		-o docker/cpi.yml \
 		-o docker/unix-sock.yml \
 		-v docker_host=unix:///var/run/docker.sock \
@@ -18,10 +18,9 @@ pushd bosh-deployment > /dev/null
 		-v internal_cidr=${NETWORK_RANGE} \
 		-v internal_gw=${NETWORK_GW} \
 		-v internal_ip=${BOSH_DIRECTOR_IP} \
-		-v network=${NETWORK_NAME} > bosh-director.yml
+		-v network=${NETWORK_NAME} \
+		--vars-store=bosh-creds.yml --state=state.json
 		
-	bosh create-env bosh-director.yml --vars-store=bosh-creds.yml --state=state.json	
-
 	bosh int bosh-creds.yml --path /director_ssl/ca > ~/ca.crt	      
 	bosh -e "${BOSH_DIRECTOR_IP}" --ca-cert ~/ca.crt alias-env "${BOSH_ENVIRONMENT}"
 popd  > /dev/null
@@ -36,17 +35,28 @@ EOF
 # ================== 
 # deploy cf
 # ==================
-source ~/boshenv
+source ~/boshenv 
 
-pushd cf-deployment
+pushd cf-deployment > /dev/null
 	bosh update-runtime-config ../bosh-deployment/runtime-configs/dns.yml --name dns --vars-store ~/dns-creds.yml  --no-redact -n
 
-	bosh int iaas-support/bosh-lite/cloud-config.yml -o ../overide.yml -v network_name=${NETWORK_NAME}  -v network_range=${NETWORK_RANGE} -v network_gw=${NETWORK_GW} -v network_static=${NETWORK_STATIC} -v dns=${DNS} >  cloud-config.yml
-	bosh update-cloud-config cloud-config.yml -n
+	bosh update-cloud-config -n iaas-support/bosh-lite/cloud-config.yml \
+			-o ../overide.yml \
+			-v network_name=${NETWORK_NAME} \
+			-v network_range=${NETWORK_RANGE} \
+			-v network_gw=${NETWORK_GW} \
+			-v network_static=${NETWORK_STATIC} \
+			-v dns=${DNS} 
 
 	export STEMCELL_VERSION=$(bosh interpolate cf-deployment.yml --path=/stemcells/alias=default/version)
 	bosh upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-xenial-go_agent?v=${STEMCELL_VERSION}
 
-	bosh -d cf deploy cf-deployment.yml -o operations/bosh-lite.yml -o operations/use-compiled-releases.yml -v system_domain=$CFDOMAIN --vars-store ~/cf-creds.yml  -n
-popd
+	bosh -d cf deploy cf-deployment.yml \
+		-o operations/bosh-lite.yml \
+		-o operations/enable-nfs-volume-service.yml \
+		-o operations/use-compiled-releases.yml \
+		-v system_domain=$CFDOMAIN \
+		--vars-store ~/cf-creds.yml  -n
+		
+popd > /dev/null
 
